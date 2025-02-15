0a14f56d8a0338912bcf0e87bb3d2a9c956317e9
anhhtpn00019 <anhhtpn00019@gmail.com>
2025-02-15 16:13:44 +0700

    Đăng nhập,  đăng ký chức năng

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,31 @@
 <?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController as AuthAdminController;
+use App\Http\Controllers\Clients\AuthController as AuthClientController;
+use App\Http\Controllers\Admin\HomeController;
 
 
-// Client 
 
-    // giỏ hàng 
-    
-     // Thanh toán 
- 
-    // voucher 
-    
+Route::get('/dang-nhap', [AuthClientController::class, 'showLoginForm'])->name('login');
+Route::post('/xu-ly-dang-nhap', [AuthClientController::class, 'login'])->name('login.store');
+Route::get('/dang-ky', [AuthClientController::class, 'showRegistrationForm'])->name('register');
+Route::post('/xu-ly-dang-ky', [AuthClientController::class, 'register'])->name('register.store');
+Route::get('/dang-xuat', [AuthClientController::class, 'logout'])->name('logout')->middleware('auth');
+Route::get('/authorized/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('/authorized/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+// Client
+
+    // giỏ hàng
+
+     // Thanh toán
+
+    // voucher
+
     // Đơn hàng
 
     //Tài khoản
-   
 
-// Admin 
 
-// authUser:1 => admin 
+// Admin
+
+// authUser:1 => admin
